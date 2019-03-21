1a5786ddfc77d504277af504d369908e5542f219
Will Jones <willjones2010@gmail.com>
2019-03-21 23:16:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ end
 
 def take_a_number(array,string)
   array.push(string)
-  puts "#{string}, #{array.index(string)+1}"
+  puts "Welcome #{string}, you are number #{array.index(string)+1} in line"
 end
